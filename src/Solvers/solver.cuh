381264e57a7a11bcfaa1f8a068a16ab01916d5f2
mohsensadr <mohsen.sadr91@gmail.com>
2025-08-30 17:42:10 +0000

    update

--- src/Solvers/solver.cuh
+++ src/Solvers/solver.cuh
@@ -4,10 +4,6 @@
 #include <cuda_runtime.h>
 #include <Containers/field_container.cuh>
 
-// Constants
-#define MAX_ITERS 200
-#define TOL 1e-5f
-
 // Main solver function
 void solve_poisson_periodic(FieldContainer& fc);
 
